ee43225f086a8ea5d9aa4d9865b7c27af184d57a
aelsen <antoniaelsen@gmail.com>
2015-09-19 16:54:52 -0400

    add quit option to human_follower script

--- warmup_project/nodes/human_follow.py
+++ warmup_project/nodes/human_follow.py
@@ -19,9 +19,10 @@ class HumanFollow(object):
 		self.target_distance = target_distance
 
 
-class Runner(object)
+class Runner(object):
 	def __init__(self):
-		self.speed = 1
+		self.done = False
+		self.speed = .5
 		self.linear = 0
 		self.angular = 0
 
@@ -29,28 +30,64 @@ class Runner(object)
 		rospy.init_node('human_follow')
 
 		self.pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
-		self.rospy.Subscriber("scan", LaserScan, self.process_scan)
+		rospy.Subscriber("scan", LaserScan, self.process_scan)
 
 		self.hfollower = HumanFollow(.5)
 
-	def process_scan(self):
-		print 
+	
+
+	def find_human(self):
+		pass
+
+	def process_key(self):
+		print "processing key"
+		tty.setraw(sys.stdin.fileno())
+		select.select([sys.stdin], [], [], 0)
+		key = sys.stdin.read(1)
+		termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.settings)
+		if (key == 'z'):
+			self.done = True
+		elif (key == 'w'):
+			self.linear = 1
+
+	def process_scan(self, scan):
+		self.ranges = scan.ranges
+		self.find_human()
+		
 
 	def run(self):
 		r = rospy.Rate(10)
 
-		while not isDone and not rospy.is_shutdown():
+		while not self.done and not rospy.is_shutdown():
+			key = self.process_key()
+
+
+			# Move.
+			print "linear: " + str(self.linear) + \
+			", angular: " + str(self.angular)
 			twist = Twist()
-			twist.linear.x = self.linear
+
+			twist.linear.x = self.linear * self.speed
 			twist.linear.y = 0
 			twist.linear.z = 0
 			# if distance < wallDistance: postive z 
 			twist.angular.x = 0 
 			twist.angular.y = 0
-			twist.angular.z = self.angular
+			twist.angular.z = self.angular * self.speed
 
-			pub.publish(twist)
-			r.sleep()
+			self.pub.publish(twist)
+
+		# Quit.
+		twist = Twist()
+		twist.linear.x = 0
+		twist.linear.y = 0
+		twist.linear.z = 0
+		# if distance < wallDistance: postive z 
+		twist.angular.x = 0 
+		twist.angular.y = 0
+		twist.angular.z = 0
+
+		self.pub.publish(twist)
 
 if __name__ == '__main__':
 	node = Runner()
